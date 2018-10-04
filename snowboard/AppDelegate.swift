43d5325366fabcaae3e9d3fdac92caa0e56e1e32
Jan Jaros <jjaros@vccp.com>
2018-10-04 15:59:50 +0200

    Init

--- snowboard/AppDelegate.swift
+++ snowboard/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GoogleMaps
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -39,6 +40,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    func applicationDidFinishLaunching(_ application: UIApplication) {
+        GMSServices.provideAPIKey("AIzaSyBsYIO8jjDIHsowzSKITpHq-vUq7kK_JV8")
     }
 
 
