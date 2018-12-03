27ff18a800e40e1a3e1b0cbbaf04f784282d10b1
Jan Jaros <jjaros@vccp.com>
2018-12-03 12:41:57 +0100

    gMaps

--- snowboard/AppDelegate.swift
+++ snowboard/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import GoogleMaps
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        GMSPlacesClient.provideAPIKey("AIzaSyBsYIO8jjDIHsowzSKITpHq-vUq7kK_JV8")
+        GMSServices.provideAPIKey("AIzaSyBsYIO8jjDIHsowzSKITpHq-vUq7kK_JV8")
+        
         return true
     }
 
@@ -41,11 +45,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-    
-    func applicationDidFinishLaunching(_ application: UIApplication) {
-        GMSServices.provideAPIKey("AIzaSyBsYIO8jjDIHsowzSKITpHq-vUq7kK_JV8")
-    }
-
 
 }
 
